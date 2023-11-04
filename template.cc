842d939faefc856433358eb934c2b9772a953f0d
Takuya Saito <72685794+shuviru@users.noreply.github.com>
2023-11-04 20:23:28 +0900

    マクロを追加

--- template.cc
+++ template.cc
@@ -1,9 +1,17 @@
 #include <bits/stdc++.h>
+using namespace std;
 #define inp(type, name) type name; cin >> name;
 #define inpv(type, name, size) vector<type> name(size); for(auto &elem : name) cin >> elem;
-using namespace std;
-
-
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+#define REP(i, n) for (auto i: views::iota(0, n))
+#define FOR(i, a, b) for (auto i: views::iota(a, b))
+#define SORT(v) ranges::sort(v)
+#define RSORT(v) ranges::sort(v, greater<ll>())
+#define FILTER(v, lambda) view::filter(v, lambda)
+#define MAP(v, lambda) view::transform(v, lambda) 
+#define ll long long
+const ll INF = 1LL << 60;
 #define endl "\n"
 
 void solve() {
